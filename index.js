ce1b0432085a78541fbad3189e56df8b72b3a2c8
Mustafa Alperen Karakuş <92369384+m-alperen-karakus@users.noreply.github.com>
2022-11-07 21:16:39 +0300

    dasdasdasd

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const { degrees, PDFDocument, rgb, StandardFonts } = PDFLib;
+
 let input = document.getElementById('input')
 let length = document.getElementById('length')
 let volume = document.getElementById('volume')
 let mass = document.getElementById('mass')
 let convert = document.getElementById('convert')
 
+let customerName = document.getElementById('customerName')
+let save = document.getElementById('save')
 
 convert.addEventListener('click', converter)
+save.addEventListener("click", modifyPdf)
 
 
 function converter(){
@@ -22,4 +27,31 @@ function converter(){
     mass.textContent = inputValue + " kilos = " +pound +" pounds" + " | " + inputValue +" pound = "+ kilogram +" kilos"
 
 }
-converter()
+// converter()
+
+
+async function modifyPdf() {
+  const url = './ilovepdf_merged.pdf'
+  const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+
+  const pdfDoc = await PDFDocument.load(existingPdfBytes)
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+  const pages = pdfDoc.getPages()
+  const firstPage = pages[0]
+  const { width, height } = firstPage.getSize()
+  firstPage.drawText(customerName.value, {
+    x: 150,
+    y: height / 2 + 300,
+    size: 50,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(0),
+  })
+
+  const pdfBytes = await pdfDoc.save()
+
+  download(pdfBytes, "zaha-film-sözleşme", "application/pdf")
+}
+
+
